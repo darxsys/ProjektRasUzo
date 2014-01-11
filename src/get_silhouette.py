d5fe37b5d7a2b1c572d7d82b40a8d3928dd54eb2
drle <drle@drle.(none)>
2014-01-11 15:08:03 +0100

    Changed names due the wrong naming convention in get_silhouette

--- src/get_silhouette.py
+++ src/get_silhouette.py
@@ -5,20 +5,20 @@ import sys
 ############################################################################
 # get_silhouette() parameters:
 #
-# 	personFile     - file containing an image with a person
-# 	backgroundFile - file containing an image with the same background as
-#					 in personFile
+#   person_file     - file containing an image with a person
+#   background_file - file containing an image with the same background as
+#                     in personFile
 #
-#	Optional:
-#	 approach      - determines gray ('g') or colour ('c') approach
-#				     	(default = 'c')
-# 	 threshType    - determines whether median ('m') or mean ('a') is used 
-#					 for image thresholding
-#					 	(default = 'm')
-# 	 threshold 	   - value added to median/mean for image thresholding 
-#				     	(default = 25)
-# 	 outFile       - silhouette output file
-#			         	(default = silhouette is shown for 5 seconds)
+#   Optional:
+#    approach       - determines gray ('g') or colour ('c') approach
+#                       (default = 'c')
+#    thresh_type    - determines whether median ('m') or mean ('a') is used 
+#                     for image thresholding
+#                       (default = 'm')
+#    threshold      - value added to median/mean for image thresholding 
+#                       (default = 25)
+#    out_file       - determines whether the silhouette is saved to a file
+#                       (default = None)
 #
 ############################################################################
 
@@ -26,15 +26,19 @@ import sys
 ############################################################################
 # PUBLIC
 
-def get_silhouette(personFile, backgroundFile, approach = 'c', threshType = 'm',
-	threshold = 25, outFile = None):
+def get_silhouette(person_file, background_file, approach = 'c', thresh_type = 'm',
+	threshold = 25, out_file = None):
+
+	# subtracting the background to get the person
 
 	if approach == 'g':
-		diff = _gray_approach(personFile, backgroundFile)
+		diff = _gray_approach(person_file, background_file)
 	else:
-		diff = _color_approach(personFile, backgroundFile)
+		diff = _colour_approach(person_file, background_file)
 
-	_get_largest_contour(diff, threshType, threshold, outFile)
+	# finding the silhouette
+
+	return _get_largest_contour(diff, thresh_type, threshold, out_file)
 
 ############################################################################
 
@@ -42,89 +46,89 @@ def get_silhouette(personFile, backgroundFile, approach = 'c', threshType = 'm',
 ############################################################################
 # PRIVATE
 
-def _color_approach(personFile, backgroundFile):
+def _colour_approach(person_file, background_file):
 
 	# reading images from input files
 
-	person = cv.imread(personFile)
+	person = cv.imread(person_file)
 	_check(person, error = "no person file found")
 
-	background = cv.imread(backgroundFile)
+	background = cv.imread(background_file)
 	_check(background, error = "no background file found")
 
 	# spliting images to red, green & blue components
 
-	personC = [person[:,:,0], person[:,:,1], person[:,:,2]]
-	backgroundC = [background[:,:,0], background[:,:,1], background[:,:,2]]
+	person_c = [person[:,:,0], person[:,:,1], person[:,:,2]]
+	background_c = [background[:,:,0], background[:,:,1], background[:,:,2]]
 
 	# subtracting images by component
 
-	diffC = []
+	diff_c = []
 
-	diffC.append(cv.absdiff(personC[0], backgroundC[0]))
-	diffC.append(cv.absdiff(personC[1], backgroundC[1]))
-	diffC.append(cv.absdiff(personC[2], backgroundC[2]))
+	diff_c.append(cv.absdiff(person_c[0], background_c[0]))
+	diff_c.append(cv.absdiff(person_c[1], background_c[1]))
+	diff_c.append(cv.absdiff(person_c[2], background_c[2]))
 
 	# applying Gaussian blur to each component (reducing noise)
 
-	diffC[0] = cv.GaussianBlur(diffC[0], (5, 5), 0)
-	diffC[1] = cv.GaussianBlur(diffC[1], (5, 5), 0)
-	diffC[2] = cv.GaussianBlur(diffC[2], (5, 5), 0)
+	diff_c[0] = cv.GaussianBlur(diff_c[0], (5, 5), 0)
+	diff_c[1] = cv.GaussianBlur(diff_c[1], (5, 5), 0)
+	diff_c[2] = cv.GaussianBlur(diff_c[2], (5, 5), 0)
 
 	# merging components to a grey image 
 	# cv.add() is a saturated operation (250 + 10 = 260 => 255)
 
-	diff = cv.add(cv.add(diffC[0], diffC[1]), diffC[2])
+	diff = cv.add(cv.add(diff_c[0], diff_c[1]), diff_c[2])
 
 	# applying Gaussian blur again
 
-	diffB = cv.GaussianBlur(diff, (11, 11), 0)
+	diff_b = cv.GaussianBlur(diff, (11, 11), 0)
 
-	return diffB
+	return diff_b
 
-def _gray_approach(personFile, backgroundFile):
+def _gray_approach(person_file, background_file):
 
 	# reading images from input files in gray scale
 
-	person = cv.imread(personFile, cv.CV_LOAD_IMAGE_GRAYSCALE)
+	person = cv.imread(person_file, cv.CV_LOAD_IMAGE_GRAYSCALE)
 	_check(person, error = "no person file found")
 
-	background = cv.imread(backgroundFile, cv.CV_LOAD_IMAGE_GRAYSCALE)
+	background = cv.imread(background_file, cv.CV_LOAD_IMAGE_GRAYSCALE)
 	_check(background, error = "no background file found")
 
 	# applying Gaussian blur to images
 
-	personB = cv.GaussianBlur(person, (5, 5), 0)
-	backgroundB = cv.GaussianBlur(background, (5, 5), 0)
+	person_b = cv.GaussianBlur(person, (5, 5), 0)
+	background_b = cv.GaussianBlur(background, (5, 5), 0)
 
 	# subtracting images
 
-	diff = cv.absdiff(personB, backgroundB)
+	diff = cv.absdiff(person_b, background_b)
 
 	# applying Gaussian blur again
 
-	diffB = cv.GaussianBlur(diff, (11, 11), 0)
+	diff_b = cv.GaussianBlur(diff, (11, 11), 0)
 
-	return diffB
+	return diff_b
 
-def _get_largest_contour(diff, threshType, threshold, outFile):
+def _get_largest_contour(diff, thresh_type, threshold, out_file):
 
 	# calculating median/mean
 
-	tr = threshold
+	thr = threshold
 
-	if threshType == 'a':
-		tr += _get_mean(diff)
+	if thresh_type == 'a':
+		thr += _get_mean(diff)
 	else:
-		tr += _get_median(diff)
+		thr += _get_median(diff)
 
 	# thresholding the image (result is a black & white image)
 
-	ret, diffT = cv.threshold(diff, tr, 255, cv.THRESH_BINARY)
+	ret, diff_t = cv.threshold(diff, thr, 255, cv.THRESH_BINARY)
 
 	# finding the largest contour
 
-	contours, hieararchy = cv.findContours(diffT, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+	contours, hieararchy = cv.findContours(diff_t, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
 	max_ = [0, 0]
 	rows, cols = diff.shape
@@ -140,19 +144,17 @@ def _get_largest_contour(diff, threshType, threshold, outFile):
 	silhouette = np.zeros((rows, cols, 1), np.uint8)
 	cv.drawContours(silhouette, contours, max_[0], (255, 255, 255), -1)
 
-	# saving image to file / showing it in a window
+	# saving image to file if a file is given
 
-	if outFile is None:
-		cv.namedWindow('silhouette', 0)
-		cv.imshow('silhouette', silhouette)
-		cv.waitKey(5000)
-		cv.destroyAllWindows()
-	else:
-		cv.imwrite(outFile, silhouette)
+	if out_file is not None:
+		cv.imwrite(out_file, silhouette)
 
+	# returning the silhouette
+
+	return silhouette
+
+# finds the median intensity value of a gray image
 def _get_median(image):
-	# finds the median intensity value of an image
-
 	arr = []
 	rows, colls = image.shape
 	for i in range(rows):
@@ -161,9 +163,8 @@ def _get_median(image):
 
 	return np.median(arr)
 
+# finds the mean intensity value of a gray image
 def _get_mean(image):
-	# finds the mean intensity value of an image
-
 	arr = []
 	rows, colls = image.shape
 	for i in range(rows):
@@ -172,9 +173,8 @@ def _get_mean(image):
 
 	return np.mean(arr)
 
+# checks if the variable is none
 def _check(var, error):
-	# checks if the variable is none
-
 	if var is None:
 		print(error)
 		sys.exit(1)
